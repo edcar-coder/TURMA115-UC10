5fe109220b347082b87b228c2d0d337546bc2f2e
THIAGO DE MOURA MONTEIRO MELO <a95484363@rn.senac.local>
2025-07-03 17:03:04 -0300

    feat:continuando atv-test

---
+++ Aula03/lista02/questao06/questao06.js
@@ -0,0 +1,18 @@
+// isEmpty(valor)
+// Verifica se o valor é considerado "vazio".
+
+// Entrada: "" → true
+// Entrada: [] → true
+// Entrada: null → true
+// (Tipo de retorno: boolean)
+
+function isEmpty(valor) {
+
+    if (valor.length) {
+
+    }
+    return true
+
+}
+
+module.exports = isEmpty;
